da3a7bd123687f53f51dd68c93502d25fdd2ef22
sirasistant <sirasistant@gmail.com>
2017-08-20 14:34:37 +0200

    Separated balances of the splits

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -5,12 +5,13 @@ contract Splitter {
     
     event LogRegister(address sender,address receiver1,address receiver2);
     event LogSplit(uint amount,address sender,address receiver1,address receiver2);
-    event LogWithdraw(uint amount,address creator,address beneficiary);
+    event LogWithdraw(uint amount,address beneficiary);
     
     struct Split{
         address[2] accounts;
-        uint[2] balances;
     }
+    
+    mapping(address=> uint) balances;
     
     mapping(address=> Split) splitters;
     
@@ -25,8 +26,7 @@ contract Splitter {
         assert(msg.sender != receiver1&&msg.sender != receiver2);
         assert(receiver1!= address(0)&&receiver2!= address(0));
         assert(receiver1!=receiver2);
-        assert(splitters[msg.sender].balances[0] ==0&&splitters[msg.sender].balances[1] ==0); //Avoid the owner of the split to overwrite balances of the receivers
-        
+
         Split memory newSplit;
         
         newSplit.accounts[0] = receiver1;
@@ -44,39 +44,28 @@ contract Splitter {
     returns (bool success){
         assert(msg.value>1 && msg.value%2==0);
         assert(splitters[msg.sender].accounts[0]!= address(0)&&splitters[msg.sender].accounts[1]!= address(0));
-        splitters[msg.sender].balances[0]+=(msg.value/2);
-        splitters[msg.sender].balances[1]+=(msg.value/2);
+        balances[splitters[msg.sender].accounts[0]]+=(msg.value/2);
+         balances[splitters[msg.sender].accounts[1]]+=(msg.value/2);
         LogSplit(msg.value,msg.sender, splitters[msg.sender].accounts[0],splitters[msg.sender].accounts[1]);
         return true;
     }
     
-    function balanceOf(address creator,address target)
+    function balanceOf(address target)
     public
     constant
     returns (uint amount){
-        uint balance;
-        for(uint i=0;i<splitters[creator].balances.length;i++){
-            if(splitters[creator].accounts[i]==target)
-                balance = splitters[creator].balances[i];
-        }
-        return balance;
+        return balances[target];
     }
     
-    function withdraw(address creator)
+    function withdraw()
     public 
     returns(bool success){
-        uint amount = 0;
-        uint index = 0;
-         for(uint i=0;i<splitters[creator].balances.length;i++){
-            if(splitters[creator].accounts[i]==msg.sender){
-                amount = splitters[creator].balances[i];
-                index = i;
-            }
-        }
+        uint amount = balances[msg.sender];
+       
         assert(amount>0);
         
-        splitters[creator].balances[index] = 0;
-        LogWithdraw(amount,creator,msg.sender);
+        balances[msg.sender] = 0;
+        LogWithdraw(amount,msg.sender);
         msg.sender.transfer(amount);
         return true;
     }
@@ -90,8 +79,8 @@ contract Splitter {
     function getSplit(address creator)
     public
     constant
-    returns(address[2] accounts, uint[2] balances){
-        return (splitters[creator].accounts,splitters[creator].balances);
+    returns(address[2] accounts){
+        return (splitters[creator].accounts);
     }
     
 }
--- test/splitter.js
+++ test/splitter.js
@@ -16,8 +16,8 @@ contract('Splitter', function (accounts) {
     return instance.register(receivers[0],receivers[1],{from:accounts[1]}).then(()=>{
       return instance.getSplit(accounts[1],{from:accounts[1]})
       .then(split=>{
-        assert.equal(split[0][0],receivers[0]);
-        assert.equal(split[0][1],receivers[1]);
+        assert.equal(split[0],receivers[0]);
+        assert.equal(split[1],receivers[1]);
       })
     })
   });
@@ -26,17 +26,17 @@ contract('Splitter', function (accounts) {
     var accountsToSplit = [accounts[1], accounts[2]];
     var balances = [];
     var amountToSend = 100;
-    return Promise.all(accountsToSplit.map((account) => instance.balanceOf.call(accounts[0],account)))
+    return Promise.all(accountsToSplit.map((account) => instance.balanceOf.call(account)))
       .then((_balances) => {
         balances = _balances;
         return instance.split({ from: accounts[0], value: amountToSend });
-      }).then(() => Promise.all(accountsToSplit.map((account) => instance.balanceOf.call(accounts[0],account))))
+      }).then(() => Promise.all(accountsToSplit.map((account) => instance.balanceOf.call(account))))
       .then((newBalances) => {
         assert.equal(newBalances[0].toString(10), balances[0].add(amountToSend / 2).toString(10),"Did not split correctly to the first receiver");
         assert.equal(newBalances[1].toString(10), balances[1].add(amountToSend / 2).toString(10),"Did not split correctly to the second receiver");
-        return Promise.all(accountsToSplit.map((account)=>instance.withdraw(accounts[0],{from:account})))
+        return Promise.all(accountsToSplit.map((account)=>instance.withdraw({from:account})))
       }).then(()=>{
-        return Promise.all(accountsToSplit.map((account) => instance.balanceOf.call(accounts[0],account)))
+        return Promise.all(accountsToSplit.map((account) => instance.balanceOf.call(account)))
       }).then((balances)=>{
         assert.equal(balances[0].toString(10),"0","Did not set the balance to 0 after withdraw");
         assert.equal(balances[1].toString(10),"0","Did not set the balance to 0 after withdraw");
